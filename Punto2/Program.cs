9f2aa23f0b633c9938924d87db92a65cc0613167
cflorian <camiloflorian14@gmail.com>
2024-04-05 22:48:38 -0500

    Actualizacion 2

--- Punto2/Program.cs
+++ Punto2/Program.cs
@@ -34,44 +34,24 @@ internal class Program
 
             for (int mes = 1; mes <= 12; mes++)
             {
-                Console.Write($"Ingrese la cantidad que desea ahorrar en el mes {mes}: ");
-                aporteMensual = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine($"Mes {mes}");
+                //Socio1
+                Console.Write("Ingrese la cantidad que desea ahorrar el socio 1: ");
+                aporteMensualSocio1 = Convert.ToDecimal(Console.ReadLine());
 
-                tasaMensual = (decimal)random.Next(1, 51) / 10;
-                rendimientoMensual = aporteMensual * (tasaMensual / 100);
-
-                if (tasaMensual < 3.5M)
+                if (aporteMensualSocio1 == 0)
                 {
-                    bonoMensual = aporteMensual * (decimal)BONO;
-                    bonoTotal += bonoMensual;
-                    bonoMensual = 0;
+                    multasSocio1 += 20000; // Aplicar multa de $20.000 si no aporta nada
                 }
 
-                aporteTotal += aporteMensual;
-                rendimientoTotal += rendimientoMensual;
-
-                Console.Write($"MES {mes}\n" +
-                              $"Aportes: {aporteMensual:C}\n" +
-                              $"Tasa: {tasaMensual}%\n" +
-                              $"Rendimientos: {rendimientoMensual:C}\n" +
-                              $"Bono: {bonoMensual:C}\n" +
-                              $"---------------------------------------\n" +
-                              $" \n");
-            }
-
-            aporteTotalNeto = rendimientoTotal + aporteTotal + bonoTotal;
-
-            Console.Write($"Aportes totales: {aporteTotal:C}\n" +
-                          $"Rendimientos totales: {rendimientoTotal:C}\n" +
-                          $"Bonos totales: {bonoTotal:C}\n" +
-                          "--------------------------------\n" +
-                          $"TOTAL NETO: {aporteTotalNeto:C}\n" +
-                          $" \n");
-
-
-            Console.WriteLine("¿Desea ingresra a la natillera para el siguiente año? (s/n)");
-            continuar = Console.ReadLine().ToLower();
-            if (continuar == "n") volver = false;
-        }
+                if (aporteMensualSocio1 < prestamoSocio1)
+                {
+                    Console.WriteLine("El préstamo solicitado supera el valor ahorrado. No se puede aprobar el préstamo.");
+                }
+                else
+                {
+                    Console.Write("¿Desea solicitar un préstamo? (s/n): ");
+                    string respuestaPrestamoSocio1 = Console.ReadLine().ToLower();
+                }
     }
 }
