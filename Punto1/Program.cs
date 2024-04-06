8e0fbd06d66edb53385e15bab18bdb98fc294783
cflorian <camiloflorian14@gmail.com>
2024-04-05 20:27:55 -0500

    Declaracion de Variables

---
+++ Punto1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Punto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desarrollar el juego “Adivina el número” en un programa de C#. El juego es muy sencillo, consiste en adivinar un número ℕ aleatorio que el sistema escoge de un determinado rango y lo guarda en memoria sin ser revelado.
+
+            //Declaracion de Variable
+            int numeroAdivinar;
+            int numeroIntentos;
+            int numeroJugador;
+            int numeroIngresado;
+            string respuesta;
+
+        }
+    }
+}
